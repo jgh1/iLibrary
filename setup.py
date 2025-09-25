55e1f55fdde35062be53fbc47cdb7419f0bd896f
Andy <hi@legner.beer>
2025-09-25 17:59:10 +0200

    Big Fixes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding = "utf-8") as fh:
 
 setup(  # Note: no 'setuptools.' prefix here
     name = "iLibrary",
-    version = "0.0.1",
+    version = "0.0.2",
     author = "Andreas Legner",
     author_email = "iLibrary@legner.beer",
     description = "iLibrary - Tools for IBM i",
